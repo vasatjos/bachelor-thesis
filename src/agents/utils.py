940a2243cb0b4b28389b1da6aa531befe4f9c5a1
Josef Vasata <jvasata03@gmail.com>
2025-12-05 18:27:00 +0100

    Add action dicts

--- src/agents/utils.py
+++ src/agents/utils.py
@@ -2,24 +2,32 @@ from game.card import Card
 from game.card_utils import Rank, Suit
 
 
-# TODO: decide on this
-class OberWrapper(Card):
-    def __init__(self, suit: Suit, rank: Rank, change: Suit) -> None:
-        super().__init__(suit, rank)
-        self.changes_to = change
+# Action: a tuple of card + suit change, agents decides these 2 separately
+# TODO: decide if game decides validity of change or agent decides validity of change
 
-# Other option: Have actions be a tuple of card + change, agents decides these 2 separately
-# Game decides validity of change vs. agent decides validity of change
+CardIndex = int
+SuitIndex = int
+Action = tuple[CardIndex, SuitIndex]
 
-
-
-# These only work with option 2
-ACTION_TO_CARD = {
-    i: card for i, card in enumerate((Card(suit, rank) for rank in Rank for suit in Suit))
+INDEX_TO_CARD: dict[CardIndex, Card | None] = {
+    i: card
+    for i, card in enumerate(
+        (Card(suit, rank) for rank in Rank for suit in Suit), start=1
+    )
 }
+INDEX_TO_CARD[0] = None  # Draw a card
 
-CARD_TO_ACTION = {
-    card: i for i, card in enumerate((Card(suit, rank) for rank in Rank for suit in Suit))
+CARD_TO_INDEX: dict[Card | None, CardIndex] = {
+    card: i
+    for i, card in enumerate(
+        (Card(suit, rank) for rank in Rank for suit in Suit), start=1
+    )
 }
+CARD_TO_INDEX[None] = 0  # Draw a card
 
-ACTIONS = ...
+INDEX_TO_SUIT: dict[SuitIndex, Suit] = {
+    i: suit for i, suit in enumerate(Suit)
+}
+SUIT_TO_INDEX: dict[Suit, SuitIndex] = {
+    suit: i for i, suit in enumerate(Suit)
+}
