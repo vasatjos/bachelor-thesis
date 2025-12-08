74f7c9690de64acecff1b627e9bd73beee1d09d6
Josef Vasata <jvasata03@gmail.com>
2025-12-08 18:07:06 +0100

    Make MC Agent comments clearer

--- src/agents/monte_carlo.py
+++ src/agents/monte_carlo.py
@@ -10,8 +10,6 @@ from random import choice, randint
 import numpy as np
 
 
-# TODO: state: (tuple??)
-
 # No hand state:
 # Don't use hand in state at all
 
@@ -23,24 +21,24 @@ import numpy as np
 # maybe sort into buckets: 0, 1, 2+
 # still probably bit array
 
-#           hand: bit array -> u32
-#           opponent card count: u8
-#           top card: CardIndex (int)
-#           active suit: SuitIndex (int)
-#           CardEffect
-#           Effect strength
-#           played cards subset: np.array (3x4), maybe flattened is better (??)
-#           state: u8 ((probably))
 
+"""
+    hand: bit array -> u32 (but actual used size depends on hand state useage)
+    opponent card count: u8
+    top card: CardIndex (int)
+    active suit: SuitIndex (int)
+    CardEffect: IntEnum
+    Effect strength: u8
+    played cards subset: np.array (3x4), maybe flattened is better (??)
+"""
 State = tuple[
-    np.uint32 | None,
+    np.uint32,
     np.uint8,
     CardIndex,
     SuitIndex,
     CardEffect,
     np.uint8,
     np.ndarray,
-    np.uint8,
 ]
 
 
@@ -56,9 +54,14 @@ class MonteCarloAgent(BaseAgent):
         self.action_value_fn = {}
         self.num_visits = {}
 
-        self.played_cards_subset = np.zeros((3, 4))  # obers + aces + sevens
+        # obers + aces + sevens
+        # update on each step
+        self.played_cards_subset = np.zeros((3, 4))
+
         self.train_episodes = opts.episodes
-        self.hand_state_variant = ...  # None vs Simple vs Full
+        self.epsilon = opts.epsilon
+        self.gamma = opts.gamma
+        self.hand_state_option = opts.hand_state_option  # TODO: None vs Simple vs Full
 
     def choose_action(self, state: Any) -> Action:
         raise NotImplementedError("TODO: Implement choose_action for monte carlo agent")
