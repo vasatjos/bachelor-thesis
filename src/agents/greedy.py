3ba15957f4b1fc6830fe13a039655c71414a33ea
Josef Vasata <vasatjos@fit.cvut.cz>
2025-12-09 19:21:45 +0100

    Add greedy agent

---
+++ src/agents/greedy.py
@@ -0,0 +1,39 @@
+from typing import Any
+from agents.base import BaseAgent
+from agents.utils import CARD_TO_INDEX, SUIT_TO_INDEX, Action
+from game.card import Card
+from game.card_utils import Rank
+from game.game_state import find_allowed_cards
+from random import choice, randint
+
+
+class GreedyAgent(BaseAgent):
+    def choose_action(self, state: Any, hand: set[Card]) -> Action:
+        playable = tuple(find_allowed_cards(state) & hand)
+
+        if not playable:
+            return (CARD_TO_INDEX[None], SUIT_TO_INDEX[None])  # draw
+
+        card_choice = choice(playable)
+        suit_index = (
+            SUIT_TO_INDEX[card_choice.suit]
+            if card_choice.rank != Rank.OBER
+            else randint(1, 4)
+        )
+
+        return (CARD_TO_INDEX[card_choice], suit_index)
+
+    def evaluate(self) -> None:
+        raise NotImplementedError("TODO: should be simple though")
+
+    # implementations for methods below not necessary
+    def train(self) -> None:
+        pass
+
+    def save(self, path: str) -> None:
+        pass
+
+    def load(self, path: str) -> None:
+        pass
+
+
