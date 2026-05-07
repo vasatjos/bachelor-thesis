32526648e14a3ceb08bdb1b9a3895c1e50f51bee
Josef Vasata <jvasata03@gmail.com>
2026-05-07 13:50:15 +0200

    Fix job numbering

--- src/run_agents.sh
+++ src/run_agents.sh
@@ -24,26 +24,26 @@ DDQN_BASE="--episodes 500_000 --log_each 1_000 --evaluate_for 20_000 --save_each
 # ==========================================
 MC_BASE="--episodes 10_000_000_000 --log_each 50_000 --evaluate_for 1_000_000 --save_each 100_000 --seed 42 "
 
-./run_thesis.sh monte_carlo mc2 $MC_BASE  --epsilon 0.05 --gamma 0.99 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc3 $MC_BASE  --epsilon 0.1 --gamma 0.99 --every_visit --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc4 $MC_BASE  --epsilon 0.1 --gamma 0.99 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc5 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.01 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc6 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.05 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc7 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh monte_carlo mc8 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials --self_play --self_play_update_freq 10000 
+./run_thesis.sh monte_carlo mc1 $MC_BASE  --epsilon 0.05 --gamma 0.99 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc2 $MC_BASE  --epsilon 0.1 --gamma 0.99 --every_visit --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc3 $MC_BASE  --epsilon 0.1 --gamma 0.99 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc4 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.01 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc5 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.05 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc6 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh monte_carlo mc7 $MC_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials --self_play --self_play_update_freq 10000 
 
 # ==========================================
 # Q-LEARNING
 # ==========================================
 QL_BASE="--episodes 10_000_000_000 --log_each 50_000 --evaluate_for 1_000_000 --save_each 100_000 --seed 42 "
 
-./run_thesis.sh q_learning ql2 $QL_BASE  --epsilon 0.05 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh q_learning ql3 $QL_BASE  --epsilon 0.1 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh q_learning ql4 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.01 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh q_learning ql5 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.05 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh q_learning ql6 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
-./run_thesis.sh q_learning ql7 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials --self_play --self_play_update_freq 10000 
-./run_thesis.sh q_learning ql9 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.2 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql1 $QL_BASE  --epsilon 0.05 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql2 $QL_BASE  --epsilon 0.1 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql3 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.01 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql4 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.05 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql5 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
+./run_thesis.sh q_learning ql6 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.1 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials --self_play --self_play_update_freq 10000 
+./run_thesis.sh q_learning ql8 $QL_BASE  --epsilon 0.5 --epsilon_decay 0.99999 --min_epsilon 0.001 --gamma 0.99 --alpha 0.2 --hand_state_option count_truncated --truncated_hand_size 4 --played_subset specials 
 
 # ==========================================
 # REINFORCE
